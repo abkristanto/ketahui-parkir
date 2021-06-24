61051c69b525d03438a2155c6e88e2893f4b0b7a
Abraham Kristanto <abkristanto@gmail.com>
2021-06-24 11:25:21 +0700

    Add bootstrap and tailwind

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,13 +21,15 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '@/plugins/bootstrap-vue',
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ['@nuxtjs/tailwindcss'],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [],
